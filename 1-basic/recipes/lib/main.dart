869c23825206a57c8ddbc57ef8b7fd677f851501
blazemassai <jayblaze291@gmail.com>
2022-04-12 00:15:26 +0300

    mwanzo

---
+++ 1-basic/recipes/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Recipe Calculator',
+        theme: ThemeData(
+            primaryColor:  Colors.white,
+            colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black)
+        ),
+        home: MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+
+        child: Container(
+            ),
+      ),
+    );
+  }
+}
